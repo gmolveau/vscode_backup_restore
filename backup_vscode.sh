63cf60f491d50e7710c0b5f6eaf3b34b0531371a
gmolveau <2090495+gmolveau@users.noreply.github.com>
2022-05-03 17:04:24 +0200

    add backup and restore scripts

---
+++ backup_vscode.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BACKUP_FOLDER="backup_$(date +'%Y-%m-%dT%H-%M-%S')"
+BACKUP_PATH="$HOME/.vscode/$BACKUP_FOLDER"
+mkdir -p "$BACKUP_PATH/extensions"
+
+echo "download latest vscode"
+cd "$BACKUP_PATH"
+wget --content-disposition "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+
+echo "backup config.json and keybindings.json"
+CFG_FOLDER="$HOME/.config/Code/User/"
+cp "$CFG_FOLDER/settings.json" "$CFG_FOLDER/keybindings.json" "$BACKUP_PATH" 2> /dev/null
+
+echo "backup extensions"
+code --list-extensions > "$BACKUP_PATH/extensions.txt"
+cd "$BACKUP_PATH/extensions"
+awk -F. '{print "wget -O "$1"."$2".vsix https://"$1".gallery.vsassets.io/_apis/public/gallery/publisher/"$1"/extension/"$2"/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"}' "$BACKUP_PATH/extensions.txt" | bash
+
+echo "creating backup archive"
+cd "$HOME/.vscode"
+zip -r "vscode_$BACKUP_FOLDER.zip" "$BACKUP_FOLDER"
+
---
+++ restore_vscode.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# usage : first (and only) argument is the path to the zip backup file
+
+([ -z "$1" ] || [ ! -f $1 ]) && (echo "backup file not found" && exit 1)
+
+# structure of the zip backup file :
+# code_XXX_amd64.deb
+# extensions/*.vsix
+# extensions.txt
+# keybindings.json (optional)
+# settings.json
+
+WORKDIR="/tmp/$(date +'%Y-%m-%dT%H-%M-%S')"
+mkdir -p "$WORKDIR"
+echo "WORKDIR = $WORKDIR"
+unzip $1 -d "$WORKDIR"
+
+echo "installing vscode"
+sudo dpkg -i "$WORKDIR"/code*.deb
+
+echo "restoring settings.json and keybindings.json"
+cp "$WORKDIR/settings.json" "$WORKDIR/keybindings.json" "$HOME/.vscode/" 2> /dev/null
+
+echo "restoring extensions"
+for ext in "$WORKDIR"/extensions/*.vsix; do
+    code --install-extension "$ext"
+done
+
