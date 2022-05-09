f17f6db5ff67d6a48980be7d59df2314eb97e6af
gmolveau <2090495+gmolveau@users.noreply.github.com>
2022-05-09 16:18:01 +0200

    rename var

--- backup_vscode.sh
+++ backup_vscode.sh
@@ -4,11 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-deps=0
+missing=0
 for app in awk wget; do
-    ! command -v ${app} &> /dev/null && echo "$name is not installed" && deps=1
+    ! command -v ${app} &> /dev/null && echo "$name is not installed" && missing=1
 done
-[[ $deps -ne 0 ]] && exit 1
+[[ $missing -ne 0 ]] && exit 1
 
 BACKUP_FOLDER="backup_$(date +'%Y-%m-%dT%H-%M-%S')"
 BACKUP_PATH="$HOME/.vscode/$BACKUP_FOLDER"
@@ -18,32 +18,32 @@ mkdir -p "$BACKUP_PATH/extensions"
 
 function clean_up() {
     ARG=$?
-    echo "> error #$ARG occured - cleaning up..."
+    echo ">>> error #$ARG - cleaning up..."
     [ -d "$BACKUP_PATH" ] && rm -rf "$BACKUP_PATH"
     [ -f "$BACKUP_ARCHIVE_PATH" ] && rm "$BACKUP_ARCHIVE_PATH"
     exit $ARG
 }
 trap clean_up ERR SIGINT
 
-echo "download latest vscode"
+echo "> download latest vscode"
 cd "$BACKUP_PATH"
 wget -q --show-progress --content-disposition "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 # wget -q --show-progress --content-disposition "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
 # wget -q --show-progress --content-disposition "https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal"
 
-echo "backup settings.json and keybindings.json"
+echo "> backup settings.json and keybindings.json"
 CFG_FOLDER="$HOME/.config/Code/User/"
 [[ -e "$CFG_FOLDER/settings.json" ]] && cp "$CFG_FOLDER/settings.json" "$BACKUP_PATH"
 [[ -e "$CFG_FOLDER/keybindings.json" ]] && cp "$CFG_FOLDER/keybindings.json" "$BACKUP_PATH"
 
-echo "backup extensions"
+echo "> backup extensions"
 code --list-extensions > "$BACKUP_PATH/extensions.txt"
 cd "$BACKUP_PATH/extensions"
 awk -F. 'NF > 0 {print "wget -q --show-progress -O "$1"."$2".vsix https://"$1".gallery.vsassets.io/_apis/public/gallery/publisher/"$1"/extension/"$2"/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"}' "$BACKUP_PATH/extensions.txt" | bash
 
-echo "creating backup archive"
+echo "> creating backup archive"
 cd "$HOME/.vscode"
 zip -q -r "$BACKUP_ARCHIVE" "$BACKUP_FOLDER"
 
-echo "done : $BACKUP_ARCHIVE_PATH"
+echo "> done : $BACKUP_ARCHIVE_PATH"
 exit 0
