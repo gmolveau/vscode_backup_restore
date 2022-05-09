481767cff9408aace00ad1efcac6ef22cc5a45d4
gmolveau <2090495+gmolveau@users.noreply.github.com>
2022-05-09 15:19:49 +0200

    add trap and good practices

--- backup_vscode.sh
+++ backup_vscode.sh
@@ -1,23 +1,43 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 BACKUP_FOLDER="backup_$(date +'%Y-%m-%dT%H-%M-%S')"
 BACKUP_PATH="$HOME/.vscode/$BACKUP_FOLDER"
+BACKUP_ARCHIVE="vscode_$BACKUP_FOLDER.zip"
+BACKUP_ARCHIVE_PATH="$HOME/.vscode/$BACKUP_ARCHIVE"
 mkdir -p "$BACKUP_PATH/extensions"
+
+function clean_up() {
+    ARG=$?
+    echo "> error #$ARG occured - cleaning up..."
+    [ -d "$BACKUP_PATH" ] && rm -rf "$BACKUP_PATH"
+    [ -f "$BACKUP_ARCHIVE_PATH" ] && rm "$BACKUP_ARCHIVE_PATH"
+    exit $ARG
+}
+trap clean_up ERR
 
 echo "download latest vscode"
 cd "$BACKUP_PATH"
-wget --content-disposition "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+wget -q --show-progress --content-disposition "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+# wget -q --show-progress --content-disposition "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
+# wget -q --show-progress --content-disposition "https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal"
 
 echo "backup settings.json and keybindings.json"
 CFG_FOLDER="$HOME/.config/Code/User/"
-cp "$CFG_FOLDER/settings.json" "$CFG_FOLDER/keybindings.json" "$BACKUP_PATH" 2> /dev/null
+[[ -e "$CFG_FOLDER/settings.json" ]] && cp "$CFG_FOLDER/settings.json" "$BACKUP_PATH"
+[[ -e "$CFG_FOLDER/keybindings.json" ]] && cp "$CFG_FOLDER/keybindings.json" "$BACKUP_PATH"
 
 echo "backup extensions"
-code --list-extensions > "$BACKUP_PATH/extensions.txt"
+code --list-extensions >"$BACKUP_PATH/extensions.txt"
 cd "$BACKUP_PATH/extensions"
-awk -F. '{print "wget -O "$1"."$2".vsix https://"$1".gallery.vsassets.io/_apis/public/gallery/publisher/"$1"/extension/"$2"/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"}' "$BACKUP_PATH/extensions.txt" | bash
+awk -F. 'NF > 0 {print "wget -q --show-progress -O "$1"."$2".vsix https://"$1".gallery.vsassets.io/_apis/public/gallery/publisher/"$1"/extension/"$2"/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"}' "$BACKUP_PATH/extensions.txt" | bash
 
 echo "creating backup archive"
 cd "$HOME/.vscode"
-zip -r "vscode_$BACKUP_FOLDER.zip" "$BACKUP_FOLDER"
+zip -q -r "$BACKUP_ARCHIVE" "$BACKUP_FOLDER"
 
+echo "done : $BACKUP_ARCHIVE_PATH"
+exit 0
