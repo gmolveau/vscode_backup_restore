80d84f4914a694645865997f502e2f04a4bd05e2
gmolveau <2090495+gmolveau@users.noreply.github.com>
2022-07-08 14:04:28 +0200

    add cleanup on EXIT, change folder location

--- backup_vscode.sh
+++ backup_vscode.sh
@@ -10,23 +10,31 @@ for app in awk wget; do
 done
 [[ $missing -ne 0 ]] && exit 1
 
+function clean_up() {
+    echo ""
+    echo ">>> cleaning up..."
+    [ -d "$BACKUP_PATH" ] && rm -rf "$BACKUP_PATH"
+}
+trap clean_up EXIT
+
+function clean_on_error() {
+    ARG=$?
+    echo ""
+    echo ">>> error #$ARG - cleaning up..."
+    [ -f "$BACKUP_ARCHIVE_PATH" ] && rm "$BACKUP_ARCHIVE_PATH"
+    exit $ARG
+}
+trap clean_on_error ERR SIGINT
+
+BACKUPS_FOLDER="$HOME/.vscode/backups"
 BACKUP_FOLDER="backup_$(date +'%Y-%m-%dT%H-%M-%S')"
-BACKUP_PATH="$HOME/.vscode/$BACKUP_FOLDER"
+BACKUP_PATH="$BACKUPS_FOLDER/$BACKUP_FOLDER"
 BACKUP_ARCHIVE="vscode_$BACKUP_FOLDER.zip"
 BACKUP_ARCHIVE_PATH="$HOME/.vscode/$BACKUP_ARCHIVE"
 mkdir -p "$BACKUP_PATH/extensions"
 
 echo "> add restore script"
-cp restore_vscode.sh "$BACKUP_PATH/"
-
-function clean_up() {
-    ARG=$?
-    echo ">>> error #$ARG - cleaning up..."
-    [ -d "$BACKUP_PATH" ] && rm -rf "$BACKUP_PATH"
-    [ -f "$BACKUP_ARCHIVE_PATH" ] && rm "$BACKUP_ARCHIVE_PATH"
-    exit $ARG
-}
-trap clean_up ERR SIGINT
+cp restore_vscode.sh "$BACKUPS_FOLDER/"
 
 echo "> download latest vscode"
 cd "$BACKUP_PATH"
@@ -45,8 +53,8 @@ cd "$BACKUP_PATH/extensions"
 awk -F. 'NF > 0 {print "wget -q --show-progress -O "$1"."$2".vsix https://"$1".gallery.vsassets.io/_apis/public/gallery/publisher/"$1"/extension/"$2"/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"}' "$BACKUP_PATH/extensions.txt" | bash
 
 echo "> creating backup archive"
-cd "$BACKUP_PATH"
-zip -q -r "$BACKUP_ARCHIVE" .
+cd "$BACKUPS_FOLDER"
+zip -q -r "$BACKUP_ARCHIVE" "$BACKUP_FOLDER"
 
 echo "> done : $BACKUP_ARCHIVE_PATH"
 exit 0
